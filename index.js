28651ee4d73fe5b53b38ce5415b59007dd9a1f5d
qqrrna17 <qiranafikriyyah@gmail.com>
2025-10-17 10:24:40 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
-const books = {};
+// qiranafikriyah17
 
-function getBooks() {
-  return books;
-}
-
-function getBookById(id) {
-  const book = books[id];
-
-  if (!book) {
-    return null;
-  }
-
-  return book.id;
-}
-
-function saveBook(book) {
-  books[book.id] = book;
-}
-
-saveBook({ id: 'book-1', name: 'Book 1' });
-const myBooks = getBooks();
-const myBook = getBookById('book-1');
-
-console.log(myBooks);
-console.log(myBook);
+/*
+Goal tahun ini:
+1. Belajar JavaScript.
+2. Menjadi Front-End atau Back-End Developer.
+*/
